a25df27c2db37586527e9ded0685185627a3f7fd
vroosh <zaidsafadi.2005@gmail.com>
2025-10-30 20:22:47 +0300

    search ui kinda works with logic

--- src/components/CreateGame.jsx
+++ src/components/CreateGame.jsx
@@ -1,4 +1,5 @@
 import {Link} from "react-router";
+import '../styles/CreatePage.css';
 import {useEffect, useState} from "react";
 
 function Search( {searchParams, setSearchParams, setFetchFlag} ) {
@@ -15,9 +16,19 @@ function Search( {searchParams, setSearchParams, setFetchFlag} ) {
     )
 }
 
+function AnimeCard( {title, img} ) {
+    return (
+        <div>
+            <img src={img} />
+            <p>{title}</p>
+        </div>
+    )
+}
+
 function CreateGame() {
 
-    const [animes, setAnimes] = useState([]);
+    const [results, setResults] = useState([]);
+    const [selected, setSelected] = useState([]);
     const [searchParams, setSearchParams] = useState("");
     const [fetchFlag, setFetchFlag] = useState(false);
 
@@ -25,9 +36,19 @@ function CreateGame() {
         if(!fetchFlag) return;
         async function fetchSearch() {
             try {
+                setResults([]);
                 const res = await fetch(`https://api.jikan.moe/v4/anime?q=${searchParams}&order_by=scored_by&sort=desc`)
                 const data = await res.json();
-                console.log(data);
+                const sorted = data.data.sort((a, b) => b.favorites - a.favorites);
+                const top8 = sorted.slice(0, 8).map((item) => {
+                    return {
+                        id: item.mal_id,
+                        title: item.title_english,
+                        img: item.images.jpg.image_url
+                    };
+                });
+                console.log(top8)
+                setResults(top8);
             } catch(err) {
                 console.error("error fetching data:", err)
             } finally {
@@ -39,11 +60,23 @@ function CreateGame() {
 
     return (
         <div id={"createpage"}>
-            <Search
-                searchParams={searchParams}
-                setSearchParams={setSearchParams}
-                setFetchFlag={setFetchFlag}
-            />
+            <div id="create-header">
+                <img src="" alt="logo" />
+                <Search
+                    searchParams={searchParams}
+                    setSearchParams={setSearchParams}
+                    setFetchFlag={setFetchFlag}
+                />
+            </div>
+            <div id="search-results">
+                {results.length === 0
+                    ? "Please search for an anime"
+                    : results.map(result => <AnimeCard key={result.id} title={result.title} img={result.img}/>)
+                }
+            </div>
+            <div id="selected-animes">
+
+            </div>
         </div>
     )
 }
